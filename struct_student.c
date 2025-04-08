d607e96bd06895295d04244df079e2a36f869866
Roshan Thomas <roshanpattathilsep16@gmail.com>
2025-04-08 23:21:41 +0530

    Add files via upload

---
+++ struct_student.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+struct student {
+
+    int id;
+    char name;
+}
+
+int main(){
+
+    struct student
+
+    student[3]={{1,"a"},{2,"b"},{3,"c"}}
+
+    for (int i=0;i<3;i++){
+
+        printf("%d   :   %d",student[i].id,student[i].name);
+    }
+    return 0;
+    
+}
